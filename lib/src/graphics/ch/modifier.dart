bdcbcbfc4e6c20693d93236f86a207edd93f3254
Dan M <daniel@thewindmills.com.au>
2022-07-19 18:41:11 +1000

    Added more colour support

--- lib/src/graphics/ch/modifier.dart
+++ lib/src/graphics/ch/modifier.dart
@@ -18,12 +18,12 @@ class Modifier {
 
   /// Creates an escape code that will set the foreground color of a [Ch]
   static Modifier fg(Colour colour) {
-    return Modifier("$_g${_fg(colour)}m");
+    return Modifier("$_g${colour.toAnsi()}m");
   }
 
   /// Creates an escape code that will set the background color of a [Ch]
   static Modifier bg(Colour colour) {
-    return Modifier("$_g${_bg(colour)}m");
+    return Modifier("$_g${colour.toAnsi(fg:false)}m");
   }
 
   /// Creates an escape code that can set a decoration of a [Ch], such as
@@ -34,7 +34,7 @@ class Modifier {
 
   /// Creates an escape code that can set the foreground and background color of a [Ch]
   static Modifier colour({Colour fg = Colour.brightwhite, Colour bg = Colour.black}) {
-    return Modifier("\x1b[${_fg(fg)};${_bg(bg)}m");
+    return Modifier("\x1b[${fg.toAnsi()};${bg.toAnsi(fg:false)}m");
   }
 
   @override
@@ -42,49 +42,6 @@ class Modifier {
     return escapeCode;
   }
 
-  
-
-  static int _fg(Colour colour) {
-    switch (colour) {
-      case Colour.black: return 30;
-      case Colour.red: return 31;
-      case Colour.green: return 32;
-      case Colour.orange: return 33;
-      case Colour.blue: return 34;
-      case Colour.magenta: return 35;
-      case Colour.cyan: return 36;
-      case Colour.white: return 37;
-      case Colour.brightblack: return 90;
-      case Colour.brightred: return 91;
-      case Colour.brightgreen: return 92;
-      case Colour.brightorange: return 93;
-      case Colour.brightblue: return 94;
-      case Colour.brightpurple: return 95;
-      case Colour.brightcyan: return 96;
-      case Colour.brightwhite: return 97;
-    }
-  }
-
-  static int _bg(Colour colour) {
-    switch (colour) {
-      case Colour.black: return 40;
-      case Colour.red: return 41;
-      case Colour.green: return 42;
-      case Colour.orange: return 43;
-      case Colour.blue: return 44;
-      case Colour.magenta: return 45;
-      case Colour.cyan: return 46;
-      case Colour.white: return 47;
-      case Colour.brightblack: return 100;
-      case Colour.brightred: return 101;
-      case Colour.brightgreen: return 102;
-      case Colour.brightorange: return 103;
-      case Colour.brightblue: return 104;
-      case Colour.brightpurple: return 105;
-      case Colour.brightcyan: return 106;
-      case Colour.brightwhite: return 107;
-    }
-  }
 
   static int _dec(Decoration decoration) {
     switch (decoration) {
--- lib/src/graphics/enums/colour.dart
+++ lib/src/graphics/enums/colour.dart
@@ -1,22 +1,102 @@
-
-/// The colours that can be set using escape codes with 
+/// The colours that can be set using escape codes with
 /// the [Modifier] object
-enum Colour {
-  red,
-  brightred,
-  orange,
-  brightorange,
-  green,
-  brightgreen,
-  cyan,
-  brightcyan,
-  blue,
-  brightblue,
-  magenta,
-  brightpurple,
-  brightwhite,
-  white,
-  brightblack,
-  black
+class Colour {
+
+  final int code;
+  final int r;
+  final int g;
+  final int b;
+  final int gray;
+
+  const Colour._(this.code,[this.r=0, this.g=0, this.b=0, this.gray=0]);
+
+  static Colour standard(int code) {
+    return Colour._(code);
+  }
+
+  static Colour expanded(int r, int g, int b) {
+    int code = 38;
+    if (r < 0 || r > 5) {
+      throw Exception("r must be in range 0-5, not $r");
+    }
+    if (g < 0 || g > 5) {
+      throw Exception("g must be in range 0-5, not $g");
+    }
+    if (b < 0 || b > 5) {
+      throw Exception("b must be in range 0-5, not $b");
+    }
+
+    return Colour._(code,r,g,b);
+  }
+
+  static Colour grayscale(int v) {
+
+    if (v > 23) {
+      throw Exception("gray must be in range 0-23, not $v");
+    }
+
+    return Colour._(38, 5, 5, 5, v+1);
+  }
+
+  String toAnsi({bool fg = true}) {
+    int out = fg ? code : code + 10;
+    String outString = "$out";
+    if (code >= 38) {
+      int _r = r;
+      int _g = g;
+      int _b = b;
+      int rgb = 16 + (36*_r) + (6*_g) + _b + gray;
+      outString = "$out;$rgb";
+    }
+    return outString;
+  }
+
+  @override
+  int get hashCode => "$code$r$g$b$gray".hashCode;
+
+  @override
+  bool operator==(Object other) {
+    if (other is Colour) {
+      return other.hashCode == hashCode;
+    }
+    return false;
+  }
+
+  static const List<Colour> defaults = [
+    red,
+    brightred,
+    orange,
+    brightorange,
+    green,
+    brightgreen,
+    cyan,
+    brightcyan,
+    blue,
+    brightblue,
+    magenta,
+    brightmagenta,
+    brightwhite,
+    white,
+    brightblack,
+    black
+  ];
+
+  static const Colour black = Colour._(30);
+  static const Colour red = Colour._(31);
+  static const Colour green = Colour._(32);
+  static const Colour orange = Colour._(33);
+  static const Colour blue = Colour._(34);
+  static const Colour magenta = Colour._(35);
+  static const Colour cyan = Colour._(36);
+  static const Colour white = Colour._(37);
+
+  static const Colour brightblack = Colour._(90);
+  static const Colour brightred = Colour._(91);
+  static const Colour brightgreen = Colour._(92);
+  static const Colour brightorange = Colour._(93);
+  static const Colour brightblue = Colour._(94);
+  static const Colour brightmagenta = Colour._(95);
+  static const Colour brightcyan = Colour._(96);
+  static const Colour brightwhite = Colour._(97);
   
 }
